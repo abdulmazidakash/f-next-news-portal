4d899038857ae47b1a40818fb7365da40c094504
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-05-19 18:12:42 +0600

    add: card create done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Banner from "@/components/shared/Banner";
+import NewsCard from "@/components/shared/NewsCard";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="py-12">
       <Banner/>
+
+      <div className="my-12">
+        <h2 className="text-2xl font-bold mb-8">Latest News</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          <NewsCard/>
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/shared/NewsCard.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from '../ui/button'
+
+const NewsCard = () => {
+  return (
+    <div className="border p-4 rounded-md shadow-md">
+      <Link href="/">
+        <Image
+          src="https://images.unsplash.com/photo-1747134392520-e3181e0bc399?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          width={500}
+          height={360}
+          alt="City skyline"
+          priority
+		  className='mb-5 md:h-56 rounded hover:scale-105 cursor-pointer transition-all duration-200'
+        />
+      </Link>
+	  <div>
+		<h2 className="text-xl font-semibold my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, totam!</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, inventore. Dolorem ipsam animi ea vel doloremque illo placeat debitis fugiat?</p>
+          <Link href={'/'}>
+            <Button variant={'default'}>Read More</Button>
+          </Link>
+	  </div>
+    </div>
+  )
+}
+
+export default NewsCard
